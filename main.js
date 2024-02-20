50ae82e5cf16093f13a2fee65909f0f8cd3827b0
kim siyeon <apyuling01@gmail.com>
2024-02-20 23:31:32 +0900

    4 commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_key=`bd31e2922bc347eca4213e8067358fef`
 let news = []
 
 const getLatesNews = async () =>{
-    const url = new URL(`https://newsallnewsallv.netlify.app/top-headlines?`);
+    const url = new URL(`https://newsallnewsallv.netlify.app/top-headlines?country=kr `);
     
     //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_key}`);
     const response = await fetch(url);
